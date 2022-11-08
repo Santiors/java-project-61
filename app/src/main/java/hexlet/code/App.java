509424eb131ad0698b35c5ed71c66f0044498033
Gennady Vrublevsky <g.vrublevsky@geoscan.aero>
2022-11-08 22:04:32 +0300

    add method to get user name

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,19 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        System.out.println("Hello, " + getName() + "!");
+    }
+
+    public static String getName() {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        return str;
     }
 
 }
