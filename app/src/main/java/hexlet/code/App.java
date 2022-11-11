27dc47a3410da442c7982e38564f242fdd852b0b
Gennady Vrublevsky <g.vrublevsky@geoscan.aero>
2022-11-11 11:44:14 +0300

    refactor App and Even game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,18 +7,13 @@ import hexlet.code.games.Greet;
 import hexlet.code.games.Prime;
 import hexlet.code.games.Progression;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Please enter the game number and press Enter.\n"
-                + "1 - Greet\n"
-                + "2 - Even\n"
-                + "3 - Calc\n"
-                + "4 - GCD\n"
-                + "5 - Progression\n"
-                + "6 - Prime\n"
-                + "0 - Exit");
-        String gameType = Engine.getAnswer();
+        printHelloWindow();
+        String gameType = getGameType();
         System.out.println("Your choice: " + gameType);
         switch (gameType) {
             case "1":
@@ -42,6 +37,22 @@ public class App {
             default:
                 throw new RuntimeException("Unknown operator: " + gameType);
         }
+    }
+
+    private static void printHelloWindow() {
+        System.out.println("Please enter the game number and press Enter.\n"
+                + "1 - Greet\n"
+                + "2 - Even\n"
+                + "3 - Calc\n"
+                + "4 - GCD\n"
+                + "5 - Progression\n"
+                + "6 - Prime\n"
+                + "0 - Exit");
+    }
+
+    private static String getGameType() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
     }
 
 }
--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -41,10 +41,9 @@ public class Even {
     }
 
     private static boolean isEven(int number) {
-        if (number % 2 ==0) {
-            return true;
-        } else {
+        if (number % 2 != 0) {
             return false;
         }
+        return true;
     }
 }
