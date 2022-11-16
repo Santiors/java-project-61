61690a31eebca0a0cff6f33d6b9d0114315f550e
Gennady Vrublevsky <g.vrublevsky@geoscan.aero>
2022-11-16 20:43:12 +0300

    refactor Even

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -4,33 +4,44 @@ import hexlet.code.Engine;
 import hexlet.code.Utils;
 
 import static hexlet.code.Engine.NUMBER_OF_COLUMNS;
+import static hexlet.code.Engine.NUMBER_OF_ROUNDS;
 import static hexlet.code.Engine.NUMBER_OF_ROWS;
+import static hexlet.code.Engine.RANDOM_END;
+import static hexlet.code.Engine.RANDOM_START;
 import static hexlet.code.Engine.checkCorrection;
 
 public class Even {
 
     private static final String DESCRIPTION = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+    private static boolean even;
+    private static String result;
 
     public static void evenGame() {
-        boolean even;
-        String result;
         String name = Engine.getGreetings(DESCRIPTION);
         String[][] arrayOfQuestions = new String[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS];
-        for (int i = 0; i < arrayOfQuestions.length; i++) {
-            for (int j = 0; j < arrayOfQuestions[i].length - 1; j++) {
-                int randomNumber = Utils.getRandomNumber();
-                even = isEven(randomNumber);
-                if (even) {
-                    result = "yes";
-                } else {
-                    result = "no";
-                }
-                arrayOfQuestions[i][j] = "Question: " + randomNumber;
-                arrayOfQuestions[i][j + 1] = result;
-            }
+        for (int i = 0; i < NUMBER_OF_ROUNDS; i++) {
+            arrayOfQuestions[i] = generateData();
         }
         checkCorrection(name, arrayOfQuestions);
     }
+
+    private static String[] generateData() {
+        int randomNumber = Utils.getRandomNumber();
+        even = isEven(randomNumber);
+        if (even) {
+            result = "yes";
+        } else {
+            result = "no";
+        }
+        String[] questionArray = new String[2];
+        for (int i = 0; i < questionArray.length - 1; i++) {
+            questionArray[i] = "Question: " + randomNumber;
+            questionArray[i + 1] = result;
+        }
+        return questionArray;
+    }
+
+
 
     private static boolean isEven(int number) {
         if (number % 2 != 0) {
