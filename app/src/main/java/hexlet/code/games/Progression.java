84c64cf7492b620c21c373e3b5844112ca236359
Gennady Vrublevsky <g.vrublevsky@geoscan.aero>
2022-11-16 20:39:12 +0300

    refactor Progression

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -4,6 +4,7 @@ import hexlet.code.Engine;
 import hexlet.code.Utils;
 
 import static hexlet.code.Engine.NUMBER_OF_COLUMNS;
+import static hexlet.code.Engine.NUMBER_OF_ROUNDS;
 import static hexlet.code.Engine.NUMBER_OF_ROWS;
 import static hexlet.code.Engine.RANDOM_END;
 import static hexlet.code.Engine.RANDOM_START;
@@ -16,20 +17,26 @@ public class Progression {
     public static void progressionGame() {
         String name = Engine.getGreetings(DESCRIPTION);
         String[][] arrayOfQuestions = new String[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS];
-        for (int i = 0; i < arrayOfQuestions.length; i++) {
-            for (int j = 0; j < arrayOfQuestions[i].length - 1; j++) {
-                int size = Utils.getRandomNumberInRange(RANDOM_START, RANDOM_END);
-                String[] array = getProgression(Utils.getRandomNumberInRangeWithoutStartPoint(RANDOM_END),
-                        Utils.getRandomNumberInRange(1, RANDOM_END), size);
-                int position = Utils.getRandomNumberInRangeWithoutStartPoint(size);
-                String answer = array[position];
-                array[position] = "..";
-                String question = "Question: " + String.join(" ", array);
-                arrayOfQuestions[i][j] = question;
-                arrayOfQuestions[i][j + 1] = answer;
-            }
+        for (int i = 0; i < NUMBER_OF_ROUNDS; i++) {
+            arrayOfQuestions[i] = generateData();
         }
         checkCorrection(name, arrayOfQuestions);
+    }
+
+    private static String[] generateData() {
+        int size = Utils.getRandomNumberInRange(RANDOM_START, RANDOM_END);
+        String[] array = getProgression(Utils.getRandomNumberInRangeWithoutStartPoint(RANDOM_END),
+                Utils.getRandomNumberInRange(1, RANDOM_END), size);
+        int position = Utils.getRandomNumberInRangeWithoutStartPoint(size);
+        String answer = array[position];
+        array[position] = "..";
+        String question = "Question: " + String.join(" ", array);
+        String[] questionArray = new String[2];
+        for (int i = 0; i < questionArray.length - 1; i++) {
+            questionArray[i] = question;
+            questionArray[i + 1] = answer;
+        }
+        return questionArray;
     }
 
     private static String[] getProgression(int first, int step, int length) {
